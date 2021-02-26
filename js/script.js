4c7667f537d3b5574f4241df1a9d20954b40eff0
Jason Hunsaker <jasonmhunsaker@gmail.com>
2021-02-26 15:59:30 -0500

    Began the showPage function

--- js/script.js
+++ js/script.js
@@ -3,13 +3,9 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+// variables to refrence HTML elements
+const studentList = document.querySelector('.studentList');
+const linkList = document.querySelector('.link-list');
 
 
 
@@ -17,7 +13,28 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage (list, page) {
+  // create two variables which will represent the index for the first and last student on the page
+  const indexStart = (page * 9) - 9;
+  const indexEnd = page * 9; 
+  // select the element with a class of `student-list` and assign it to a variable
+  //I did that in global scope
+  // set the innerHTML property of the variable you just created to an empty string
+  studentList.innerHTML = '';
+  // loop over the length of the `list` parameter
+   for (i=0; i < studentList.length; i++) {
+      if (i >= indexStart && i <= indexEnd) {
+         let studentInfo = ``
+      }
+   }
+    // inside the loop create a conditional to display the proper students
+      // inside the conditional:
+        // create the elements needed to display the student information
+        // insert the above elements
 
+  
+   
+}
 
 
 /*
